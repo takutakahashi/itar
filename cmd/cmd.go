2a2bd77c531a6dd6dc30570446cec30798b8dc4d
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-06-10 00:36:09 +0900

    dir

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"github.com/mholt/archiver"
 	"github.com/urfave/cli"
-  "github.com/mholt/archiver"
+	"io/ioutil"
 	"os"
-  "io/ioutil"
-  "path/filepath"
-  "fmt"
+	"path/filepath"
 )
 
 var Version string = "0.9.0"
@@ -23,28 +23,33 @@ func newApp() *cli.App {
 	app.Author = "takutakahashi"
 	app.Email = "taku.takahashi120@gmail.com"
 	app.Action = func(c *cli.Context) error {
-    return makeTar(c.Args().Get(0))
+		return makeTar(c.Args().Get(0))
 	}
 	return app
 }
 
 func makeTar(archivePath string) error {
-  tarPath := archivePath + ".tar"
-  tar := archiver.NewTar()
-  tar.Archive(getFileDirList(archivePath), tarPath)
+	tarPath := archivePath + ".tar"
+	tar := archiver.NewTar()
+	filePaths := getFileDirList(archivePath)
+	fmt.Println(filePaths)
+	tar.Archive(filePaths, tarPath)
 	return nil
 }
 
 func getFileDirList(path string) []string {
-  files, err := ioutil.ReadDir(path)
-  if err != nil {
-    panic(err)
-  }
-  var paths []string
-  for _, file := range files {
-    paths = append(paths, filepath.Join(path, file.Name()))
-  }
-
-  fmt.Println(paths)
-  return paths
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, getFileDirList(filepath.Join(path, file.Name()))...)
+			continue
+		} else {
+			paths = append(paths, filepath.Join(path, file.Name()))
+		}
+	}
+	return paths
 }
